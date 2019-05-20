4d4a0b0049cc8b586f7c8276fae7c09dfcef247a
ecacerest <ecacerest@finagil.com.mx>
2019-05-20 18:57:30 -0500

    configuracion de SMTP al config

--- GeneraPersonas.vb
+++ GeneraPersonas.vb
@@ -202,8 +202,10 @@
     Private Sub EnviaError(ByVal Para As String, ByVal Mensaje As String, ByVal Asunto As String)
         If InStr(Mensaje, Asunto) = 0 Then
             Dim Mensage As New System.Net.Mail.MailMessage("InternoBI2008@cmoderna.com", Trim(Para), Trim(Asunto), Mensaje)
-            Dim Cliente As New System.Net.Mail.SmtpClient("smtp01.cmoderna.com", 26)
+            Dim Cliente As New System.Net.Mail.SmtpClient(My.Settings.SMTP, My.Settings.SMTP_port)
             Try
+                Dim Credenciales As String() = My.Settings.SMTP_creden.Split(",")
+                Cliente.Credentials = New System.Net.NetworkCredential(Credenciales(0), Credenciales(1), Credenciales(2))
                 Cliente.Send(Mensage)
             Catch ex As Exception
                 'ReportError(ex)
